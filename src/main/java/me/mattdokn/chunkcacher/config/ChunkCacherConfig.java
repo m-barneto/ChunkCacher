b63c3ce750b41a910339e62fa945f6307425b0ba
Matthew Barneto <4347791+m-barneto@users.noreply.github.com>
2021-09-11 18:57:44 -0400

    Removed forgotten debug prints

--- src/main/java/me/mattdokn/chunkcacher/config/ChunkCacherConfig.java
+++ src/main/java/me/mattdokn/chunkcacher/config/ChunkCacherConfig.java
@@ -8,16 +8,13 @@ import net.minecraft.text.TranslatableText;
 
 public class ChunkCacherConfig implements ModMenuApi {
     private static ConfigBuilder builder() {
-        System.out.println("Building config menu");
         ConfigBuilder builder = ConfigBuilder.create()
                 .setTitle(new TranslatableText("category.chunkcacher"))
                 .setEditable(true)
                 .setSavingRunnable(ModConfig::writeJson);
 
-        System.out.println("Building general category");
         ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("chunkcacher.config.general"));
 
-        System.out.println("Building unload distance config entry");
         general.addEntry(builder.entryBuilder()
                 .startIntField(new TranslatableText("chunkcacher.config.unloaddistance"), ModConfig.INSTANCE.unloadDistance)
                 .setDefaultValue(32)
@@ -28,8 +25,6 @@ public class ChunkCacherConfig implements ModMenuApi {
     }
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-
-        System.out.println("Calling builder");
         return parent -> builder().setParentScreen(parent).build();
     }
 }
